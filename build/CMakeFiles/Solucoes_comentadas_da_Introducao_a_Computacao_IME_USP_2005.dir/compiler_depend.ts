ddf205e9fff686c858f30917ce8f859a26e7ce99
vitor_ubuntu <vitorhiroshi1327@gmail.com>
2025-10-06 00:46:07 -0300

    Cmake Arrumado

--- build/CMakeFiles/Solucoes_comentadas_da_Introducao_a_Computacao_IME_USP_2005.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for Solucoes_comentadas_da_Introducao_a_Computacao_IME_USP_2005.
--- build/CMakeFiles/fn2_5.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for fn2_5.
-# This may be replaced when dependencies are built.
